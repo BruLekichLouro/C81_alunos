5bd32c7aec1391540dd62ae0781daa5373d6b0a9
Bruna Lekich Louro <brunalekich@gmail.com>
2022-11-29 12:35:41 -0300

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,32 @@
+//CRIAR canvas e cor do pincel:
+var canvas = 
+var ctx = 
+var color = 
+
+//ctx = pega o contexto do Canvas e faz modificações nele.
+
+//Configurações do ctx, 
+
+
+//Adicionar escutador de eventos:'addEventListener'
+
+
+
+//Funções:
+function myMousedown(e) {
+    
+    
+}
+
+function circle(mouseX, mouseY) {
+    
+
+}
+
+
+//atividade adicional
+function clearArea() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
